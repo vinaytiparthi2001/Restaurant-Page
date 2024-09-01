2f0e6465574356133eeb9b0c3c10dd5d5299d5fc
Vinay Reddy <130531809+vinaytiparthi2001@users.noreply.github.com>
2024-09-01 17:37:25 +0530

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+var ItemId = 0;
+function addtoCart(){
+    addItemId =+1;
+    var selectedItem = document.createElement('div');
+    selectedItem.classList.add(cartImg);
+    selectedItem.setAttribute('id',addItemId);
+    var Img = document.createElement('img');
+    img.setAttribute('src',item.children[0].currentSrc);
+    var cartItems = document.getElementById('title');
+    selectedItem.append(img);
+    cartItems.append(selectedItem);
+
+}
